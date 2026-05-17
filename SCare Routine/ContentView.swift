67389b883d3403d4fa83482ac9b74507fa3e6e8c
Ali Arif Soydas <aliarifsoydas@hotmail.com.tr>
2026-05-17 22:11:02 +0300

    Initial Commit

---
+++ SCare Routine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SCare Routine
+//
+//  Created by aaStudio on 17.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SCare Routine/SCare_RoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCare_RoutineApp.swift
+//  SCare Routine
+//
+//  Created by aaStudio on 17.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct SCare_RoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
